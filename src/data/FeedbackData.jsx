28aedb7de29b516c80bfc97c6d7fc67c2d91ddab
nathanMulugeta01 <nathanmulugetacoding@gmail.com>
2023-01-26 13:00:56 +0300

    folders added

---
+++ src/data/FeedbackData.jsx
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum animi harum fuga quasi esse?',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem eligendi iste illum quas vero dignissimos, dolore sed dolores rem sit molestias reiciendis sequi delectus accusantium, eius dolorum in minus voluptate qui natus. Esse provident excepturi sed? Amet fuga velit harum.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+  ]
+  
+  export default FeedbackData
